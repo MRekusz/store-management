028f089517bbf101f45ecb7bb404cc4e4824440c
MRekusz <maciek-x2@o2.pl>
2020-11-04 23:07:49 +0100

    tests for UserService interferace

---
+++ src/test/java/UserServiceTest.java
@@ -0,0 +1,57 @@
+import api.UserService;
+import entity.User;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserServiceTest {
+
+
+    @Test
+    public void testGetAllUser() {
+        List<User> users = new ArrayList<User>();
+        users.add(new User(1L, "user1", "pass1"));
+        users.add(new User(2L, "user3", "pass2"));
+        users.add(new User(3L, "user3", "pass3"));
+        users.add(new User(4L, "user4", "pass4"));
+
+        UserServiceImplementation userServiceImplementation = new UserServiceImplementation(users);
+        List<User> usersFromTestClass = userServiceImplementation.getAllUser();
+
+        Assert.assertEquals(users, usersFromTestClass);
+    }
+
+    @Test
+    public void testAddUser() {
+        List<User> users = new ArrayList<User>();
+        User user = new User(1l, "admin", "admin");
+        users.add(user);
+
+        UserServiceImplementation userServiceImplementation = new UserServiceImplementation();
+        userServiceImplementation.addUser(user);
+        List<User> usersFromTestClass = userServiceImplementation.getAllUsers();
+
+        Assert.assertEquals(user, usersFromTestClass);
+    }
+
+    @Test
+    public void testRemoveUser() {
+        List<User> users = new ArrayList<User>();
+        User user = new User(1L, "user1", "pass1");
+        User user1 = new User(2L, "user2", "pass2");
+        users.add(user);
+        users.add(user1);
+
+        UserServiceImplementation userServiceImplementation = new UserServiceImplementation();
+        userServiceImplementation.removeUserById(1L);
+        users.remove(user);
+        List<User> usersFromTestClass = userService.getAllUsers();
+
+        Assert.assertEquals(users, usersFromTestClass);
+
+
+    }
+
+}
