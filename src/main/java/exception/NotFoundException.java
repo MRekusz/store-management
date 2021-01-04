f2fec2bca05cfb19eae0a1209cff9cdd6fe56cb0
MRekusz <maciek-x2@o2.pl>
2021-01-05 00:51:56 +0100

    exceptions added

--- src/main/java/exception/NotFoundException.java
+++ src/main/java/exception/NotFoundException.java
@@ -2,6 +2,9 @@ package exception;
 
 public class NotFoundException extends Exception {
 
+    public NotFoundException() {
+    }
+
     public NotFoundException(String message) {
         super(message);
     }
