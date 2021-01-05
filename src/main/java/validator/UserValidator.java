8cf619a30c89d9d4031aae4b12298aa164ef7171
MRekusz <maciek-x2@o2.pl>
2021-01-05 01:27:03 +0100

    * new method in validator

--- src/main/java/validator/UserValidator.java
+++ src/main/java/validator/UserValidator.java
@@ -1,4 +1,41 @@
 package validator;
 
+import api.UserDao;
+
+import java.io.IOException;
+
 public class UserValidator {
+
+    private final int MIN_LENGTH_PASSWORD = 6;
+    private final int MIN_LENGTH_LOGIN = 4;
+
+    private static UserValidator instance = null;
+
+    public UserValidator getInstance() {
+        if (instance == null) {
+            instance = new UserValidator();
+        }
+        return instance;
+    }
+
+    private boolean isPasswordLengthEnough(String password) {
+        return password.length() >= MIN_LENGTH_PASSWORD;
+    }
+
+    private boolean isLoginLengthEnough(String login) {
+        return login.length() >= MIN_LENGTH_LOGIN;
+    }
+
+    private boolean isLoginAlreadyExists(String login) {
+        UserDao userDao = null;
+        try {
+            userDao = (UserDao) userDao.getUserByLogin(login);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (userDao == null) return false;
+
+        return true;
+    }
+
 }
