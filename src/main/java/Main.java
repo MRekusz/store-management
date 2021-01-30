56fd369fe00df5a090c12b0fb2a21df6bd8862be
MRekusz <maciek-x2@o2.pl>
2021-01-30 22:41:00 +0100

    Main - new methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
+    static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-       
+    public void startMenu() {
+        System.out.println("MANAGEMENT MENU");
+        System.out.println("1 - Zaloguj się");
+        System.out.println("2 - Zarejestruj się");
+        System.out.println("0 - Wyjdź");
+    }
+
+    public static void loggedMenu(){
+        System.out.println("MANAGEMENT MENU");
+        System.out.println("1 - Dodaj nowy product");
+        System.out.println("0 - Wyloguj się");
+    }
+
+    public static void productTypeMenu() {
+        System.out.println("1 - Dodaj buty");
+        System.out.println("2 - Dodaj ubrania");
+        System.out.println("3 - Inne");
     }
 }
