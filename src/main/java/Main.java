113b268bf5e5b5fcb8befb305acae85f6c8a4c28
MRekusz <maciek-x2@o2.pl>
2021-02-02 21:25:43 +0100

    add methods in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
-public class Main{
+import java.util.Scanner;
 
+public class Main {
+
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void startMenu() {
+        System.out.println("MANAGEMENT MENU");
+        System.out.println("1 - Zaloguj się");
+        System.out.println("2 - Zarejestruj się");
+        System.out.println("0 - Wyjdź");
+    }
+
+    public static void loggedMenu() {
+        System.out.println("MANAGEMENT MENU");
+        System.out.println("1 - Dodaj nowy product");
+        System.out.println("0 - Wyloguj się");
+    }
+
+    public static void productTypeMenu() {
+        System.out.println("1 - Dodaj buty");
+        System.out.println("2 - Dodaj ubrania");
+    }
 
     public static void main(String[] args) {
 
